23305df892738f82c4c05d0e3886397a96b63300
David H <davidhorph@hotmail.com>
2018-06-24 23:05:18 -0500

    New files.
    Hi, Alexis... 
    
    I tried to send the new files to you as a zip. 
    
    Please look over these before merging because I don't know if this is correct. 
    
    I believe if you open the Program.cs, that you will find the changes. Basically, what I did was that I changed the Addbooks to a "Book" type instead of a void and then used that as a parameter to feed into the ListBooks to add the book (which is set to default null).

---
+++ CardCatalogUpdated/ConsoleApp2/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CardCatalogUpdated
+{
+    [Serializable]
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ CardCatalogUpdated/ConsoleApp2/CardCatalog.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CardCatalogUpdated
+{
+    public class CardCatalogUpdated : Book
+    {
+        private string _filename { get; set; }
+        private List<Book> Books = new List<Book>();
+
+        public CardCatalogUpdated(string fileName)
+        {
+            _filename = fileName;
+        }
+        
+        public void ListBooks(Book addbook = null)
+        {
+            if (File.Exists(_filename))
+            {
+                try
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    Stream readStream = new FileStream(_filename, FileMode.Open);
+                    Books = (List<Book>)bf.Deserialize(readStream);
+                    readStream.Close();
+                    readStream.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Books = new List<Book>()
+            {
+                new Book{Title = "Rosemary's Baby", Author = "Ira Levin", Genre = "Horror" },
+                new Book{Title = "50 Shades of Grey", Author = "E.L.Green", Genre = "Erotica"},
+                new Book{Title = "Harry Potter", Author = "J.K. Rowling", Genre = "Young Adult"},
+                new Book{Title = "Salem's Lot", Author = "Stephen King", Genre = "Horror"},
+                new Book{Title = "Percy Jackson", Author = "Rick Riordan", Genre = "Young Adult" },
+            };
+
+
+            if (addbook == null)
+            {
+
+            }
+            else
+            {
+                Books.Add(addbook);
+            } 
+
+
+            var alphabetizedBooks = from AllBooks in Books
+                                    orderby AllBooks.Title ascending
+                                    select AllBooks;
+
+            //replaced the forEach loop with this lambda expression
+            Books.ForEach(p => Console.WriteLine("{0} written by {1}", p.Title, p.Author));
+
+        }
+
+        public Book AddBook()
+        {
+            Console.WriteLine("Please enter a Title: ");
+            string bookTitle = Console.ReadLine();
+
+            Console.WriteLine("Please enter an Author: ");
+            string bookAuthor = Console.ReadLine();
+
+            Console.WriteLine("Please enter a genre: ");
+            string bookGenre = Console.ReadLine();
+            
+            Book bookAddition = new Book() { Title = bookTitle, Author = bookAuthor, Genre = bookGenre };
+            //Books.Add(bookAddition);
+
+            return bookAddition;
+        }
+
+        public void Save()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(_filename, FileMode.Create);
+            formatter.Serialize(stream, Books);
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}
